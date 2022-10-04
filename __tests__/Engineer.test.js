9100228507b02cbcc7a7735e0708c3f6b20fbf47
awchen <awchen85@gmail.com>
2022-10-03 21:56:41 -0400

    finished engineer test, engineer

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,7 +1,18 @@
+const { default: test } = require('node:test');
 const Engineer = require('../lib/Engineer');
 
 
 test("sets github from constructor", () => {
     const employee = new Engineer("Bob", "001", "bob@gmail.com", "bobbyboi");
     expect(employee.github).toBe("bobbyboi");
+});
+
+test ("fetch github from getGithub", () => {
+    const employee = new Engineer("Bob", "001", "bob@gmail.com", "bobbyboi");
+    expect(employee.getGithub()).toBe("bobbyboi");
+});
+
+test ("fetch employees role from getRole", () => {
+    const employee = new Engineer("Bob", "001", "bob@gmail.com", "bobbyboi");
+    expect(employee.getRole()).toBe("Engineer");
 });

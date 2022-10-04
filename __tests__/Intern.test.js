ce118e7349eb670d512826b1bd892d594d43b8dc
awchen <awchen85@gmail.com>
2022-10-04 16:46:20 -0400

    added intern, intern test. test failed, then passed

---
+++ __tests__/Intern.test.js
@@ -0,0 +1,22 @@
+const Intern = require('../lib/Intern');
+
+
+test('sets school using constructor', () => {
+    const employee = new Intern("Bob", "001", "bob@gmail.com", "Hard Knocks");
+    expect(employee.school).toBe("Hard Knocks");
+});
+
+test("fetch school from getSchool", () => {
+    const employee = new Intern("Bob", "001", "bob@gmail.com", "Hard Knocks");
+    expect(employee.getSchool()).toBe("Hard Knocks");
+});
+
+test ("fetch employees role from getRole", () => {
+    const employee = new Intern("Bob", "001", "bob@gmail.com", "Hard Knocks");
+    expect(employee.getRole()).toBe("Intern");
+});
+
+test("fetch employee id from getId from employee super", () => {
+    const employee = new Intern("Bob", "001", "bob@gmail.com", "Hard Knocks");
+    expect(employee.getID()).toBe("001");
+});
